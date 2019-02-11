3393f311566eec20f9ef8686ba4acbc23cabcd00
Katrina King <kmking@mail.bradley.edu>
2019-02-10 20:19:41 -0600

    updated vue stuff
    Honestly I still don't really understand this. I need to take more time to work on javaScript.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    counter: 0
-  }
+    // message: 'Good Job!',
+    // counter: 0
+    // practiced: false
+    // name: 'Vue.js'
+
+  },
+  methods: {
+    congrat: function (event) {
+      alert('Good J     ob!')
+      if (event) {
+        alert(event.target.tagName)
+      }
+      }
+    }
+
 });
+
 
 window.app = app;
