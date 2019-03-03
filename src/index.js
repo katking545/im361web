531f95fe572b9f19da36493f0bf162e59e3c0dfc
Katrina King <kmking@mail.bradley.edu>
2019-03-03 11:17:52 -0600

    First part of js
    i finally get it!!!

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    pageName: "start"
-  }
+    pageName: "start",
+    firstName: "first",
+    lastName: "last",
+  },
+  computed: {
+      fullName: function () {
+        return this.firstName + ' ' + this.lastName
+  },
+  },
 
-  methods: {
-    // I got this from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_defaultvalue3
-    // You didnt show us how to do this you just said it as an exapmle pls give me credit for figuring it out
-    fullName: function () {
-      var x = document.getElementById("firstName");
-      var firstName = x.firstName;
-      var x = document.getElementById("lastName");
-      var lastName = x.lastName;
-      var fullName = firstName + " " + lastName;
 
-    }
-  }
+
+
+
+
 
 
 });
